58bd60a39ad48c75d06289d14375049f85b11a00
wenaltszn <91277430+wenalt@users.noreply.github.com>
2025-09-24 00:50:20 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+// pages/_app.js
+import Head from 'next/head'
 import { WagmiConfig } from 'wagmi'
-import { wagmiConfig } from '../lib/appkit' // was '@/lib/appkit'
+import { wagmiConfig } from '../lib/appkit'
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <Component {...pageProps} />
-    </WagmiConfig>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Base Lite meta assets */}
+        <link rel="icon" href="/baseicon.png" />
+        <meta property="og:image" content="/baseog.png" />
+        <meta name="theme-color" content="#0052FF" />
+        {/* Farcaster Mini-app meta kept compatible */}
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:miniapp" content="v1" />
+      </Head>
+      <WagmiConfig config={wagmiConfig}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+    </>
   )
 }
