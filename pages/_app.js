fcdc56c6c1ed2a3b38f4683a326b25e60e5d05fd
wenaltszn <91277430+wenalt@users.noreply.github.com>
2025-09-23 22:57:55 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,11 @@
-import "../styles/globals.css";
-import Head from "next/head";
-import { WagmiConfig } from "wagmi";
-import { wagmiConfig } from "../lib/appkit";
+// pages/_app.js
+import { WagmiConfig } from 'wagmi'
+import { wagmiConfig } from '@/lib/appkit'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        {/* Farcaster Mini App meta (kept for parity) */}
-        <meta name="fc:miniapp" content="1" />
-        <meta property="og:title" content="Base Lite" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/icons/app.png" />
-      </Head>
-
-      <WagmiConfig config={wagmiConfig}>
-        <div className="container">
-          {/* Blue hero bar like Celo Lite’s header but Base-flavored */}
-          <div className="hero" style={{ marginBottom: 12 }}>
-            <div className="title">Base Lite</div>
-            <div className="pill">mini-hub Superchain</div>
-          </div>
-
-          <Component {...pageProps} />
-        </div>
-      </WagmiConfig>
-    </>
-  );
+    <WagmiConfig config={wagmiConfig}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
