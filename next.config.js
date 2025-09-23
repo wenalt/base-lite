4feff7965eb3e1b21629950dbf241ea797a67550
wenaltszn <91277430+wenalt@users.noreply.github.com>
2025-09-24 01:34:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
