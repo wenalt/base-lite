030f3e772fc1f9c7ff6f942c6a0ed80443bb05c8
wenaltszn <91277430+wenalt@users.noreply.github.com>
2026-01-15 11:26:00 +0100

    Refactor webpack config to update fallback and alias
    Remove fallback configuration for several packages and add alias for 'porto'.

--- next.config.js
+++ next.config.js
@@ -3,17 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 
   webpack: (config) => {
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      '@base-org/account': false,
-      '@coinbase/wallet-sdk': false,
-      '@gemini-wallet/core': false,
-      '@metamask/sdk': false,
-      'porto/internal': false
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      porto: false,
     }
 
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
